4ecc261b7d17e464b96961e347d0626bb9149c98
徐國堂 <roberthsu2003@gmail.com>
2019-07-12 15:31:49 +0800

    modify

---
+++ web/public/js/boolean-variable.js
@@ -0,0 +1,9 @@
+var inStock;
+var shipping;
+inStock = true;
+shipping = false;
+
+var elStock = document.getElementById("stock");
+var elShip = document.getElementById("shipping");
+elStock.className = inStock;
+elShip.className = shipping;
