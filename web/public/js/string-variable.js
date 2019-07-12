4fb36dc54f3539f8015d3c6fb308de0756067967
徐國堂 <roberthsu2003@gmail.com>
2019-07-12 14:41:18 +0800

    modify

---
+++ web/public/js/string-variable.js
@@ -0,0 +1,10 @@
+var username;
+var message;
+username = "徐國堂";
+message = "大家好!午安。";
+
+
+var elName = document.getElementById("name");
+var elNote = document.getElementById("note");
+elName.textContent = username;
+elNote.textContent = message;
