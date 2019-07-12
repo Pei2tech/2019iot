566679b6d7df1fcbc54f184ffe7537eb87b8de31
徐國堂 <roberthsu2003@gmail.com>
2019-07-12 11:49:19 +0800

    modify

---
+++ web/public/js/add-content.js
@@ -0,0 +1,13 @@
+var today = new Date();
+var hourNow = today.getHours();
+console.log(hourNow);
+
+var greeting;
+if(hourNow > 18){
+     greeting = "晚安";
+}else if(hourNow > 12){
+    greeting = "午安";
+}else if (hourNow > 0){
+    greeting = "早安";
+}
+console.log(greeting);
