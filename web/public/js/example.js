020a10b7ff08d9daab8cfb4fa6b95dba1a0790c9
徐國堂 <roberthsu2003@gmail.com>
2019-07-15 09:39:43 +0800

    modify

---
+++ web/public/js/example.js
@@ -0,0 +1,13 @@
+var greeting = "Howdy";
+var name = "Molly";
+var message = ", please check your order:";
+
+var welcome = greeting + name + message;
+
+//sign
+var sign = 'Montague House';
+var tiles = sign.length;
+var subTotal = tiles * 5;
+var shipping = 7;
+var grandTotal = subTotal + shipping;
+console.log("grandTotal:" + grandTotal);
