df4aa81c0d848717757f084f008bdd067a3f0b58
徐國堂 <roberthsu2003@gmail.com>
2019-07-29 12:00:11 +0800

    modify

---
+++ web/public/section5/js/changing-content.js
@@ -0,0 +1,11 @@
+/*
+$(document).ready(function(event){
+    $('li:contains("pine")').text("almonds");
+});
+*/
+
+$(function(){
+     var $li_pine = $('li:contains("pine")');
+     $li_pine.text("almonds");
+});
+
