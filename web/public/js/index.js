47d7037f365280034d8c0c9440a65210d40554bc
徐國堂 <roberthsu2003@gmail.com>
2019-08-07 12:00:38 +0800

    modify

--- web/public/js/index.js
+++ web/public/js/index.js
@@ -1,6 +1,17 @@
 $(function(){
     $('.signIn').on('click',function(event){
-        console.log("sign in...");
+        /*
+        var txt = "";
+        txt += "<p>innerWidth: " + window.innerWidth + "</p>";
+        txt += "<p>innerHeight: " + window.innerHeight + "</p>";
+        txt += "<p>outerWidth: " + window.outerWidth + "</p>";
+        txt += "<p>outerHeight: " + window.outerHeight + "</p>";
+        console.log(txt);
+
+         */
+        var windowWidth = window.innerWidth;
+        var windowHeight = window.innerHeight;
+        var newDom = $("<div></div>");
         event.preventDefault();
     });
 });
