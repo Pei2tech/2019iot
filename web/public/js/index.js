d68830878d76c2f2adaa3a6742ec15929d7e8e65
徐國堂 <roberthsu2003@gmail.com>
2019-08-07 13:16:41 +0800

    modify

--- web/public/js/index.js
+++ web/public/js/index.js
@@ -1,17 +1,18 @@
 $(function(){
     $('.signIn').on('click',function(event){
-        /*
-        var txt = "";
-        txt += "<p>innerWidth: " + window.innerWidth + "</p>";
-        txt += "<p>innerHeight: " + window.innerHeight + "</p>";
-        txt += "<p>outerWidth: " + window.outerWidth + "</p>";
-        txt += "<p>outerHeight: " + window.outerHeight + "</p>";
-        console.log(txt);
 
-         */
-        var windowWidth = window.innerWidth;
-        var windowHeight = window.innerHeight;
-        var newDom = $("<div></div>");
+        let windowWidth = window.innerWidth;
+        let windowHeight = window.innerHeight;
+        let $newDom = $("<div></div>");
+        $newDom.innerHeight(windowHeight).innerWidth(windowWidth);
+        $newDom.css({
+            backgroundColor:'#aaa',
+            position:'absolute',
+            top:'0px',
+            let:'0px',
+            opacity:0.5,
+        });
+        $("body").append($newDom);
         event.preventDefault();
     });
 });
