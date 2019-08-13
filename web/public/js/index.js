1e240f2b8e4d6f3c2518f9b77d784aff225a2f86
徐國堂 <roberthsu2003@gmail.com>
2019-08-13 12:02:58 +0800

    modify

--- web/public/js/index.js
+++ web/public/js/index.js
@@ -15,15 +15,18 @@ $(function(){
 
 $(function(){
     var menuState = false;
-    $("#menu").on("click",function(event){
+    $("#menu").on("click touchstart",function(event){
         menuState = !menuState;
 
         if (menuState) {
             console.log("menu open");
             $(this).attr("src","images/close.png");
+            $("nav>ul").css("display","block");
         }else{
             console.log("menu close");
             $(this).attr("src","images/menu.png");
+            $("nav>ul").css("display","none");
         }
+        event.preventDefault();
     });
 });
