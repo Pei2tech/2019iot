cee5b18711cb76e84e951d047cad7095bb7203a5
徐國堂 <roberthsu2003@gmail.com>
2019-08-13 11:37:14 +0800

    modify

--- web/public/js/index.js
+++ web/public/js/index.js
@@ -17,10 +17,13 @@ $(function(){
     var menuState = false;
     $("#menu").on("click",function(event){
         menuState = !menuState;
+
         if (menuState) {
             console.log("menu open");
+            $(this).attr("src","images/close.png");
         }else{
             console.log("menu close");
+            $(this).attr("src","images/menu.png");
         }
     });
 });
