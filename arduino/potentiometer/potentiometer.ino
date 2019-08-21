e45a7dd2fe054e014ecbfe944583d79f986cc843
徐國堂 <roberthsu2003@gmail.com>
2019-08-21 12:06:42 +0800

    Update potentiometer.ino

--- arduino/potentiometer/potentiometer.ino
+++ arduino/potentiometer/potentiometer.ino
@@ -1,6 +1,9 @@
 #define RedPin 6
 #define potPin A0
 unsigned long noteTime = millis();
+unsigned long note1Time = millis();
+int valueOf8bit;
+boolean ledState = false;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
@@ -11,7 +14,7 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   doSomeThing();
- 
+  blinkLed();
 }
 
 void doSomeThing() {
@@ -21,9 +24,24 @@ void doSomeThing() {
     noteTime = currentTime;
     
     int potValue = analogRead(potPin);
-    int valueOf8bit = (float)potValue / 1023 * 255; 
+    valueOf8bit = (float)potValue / 1023 * 255; 
     Serial.println(valueOf8bit);
     analogWrite(RedPin, valueOf8bit);
   }
  
+}
+
+void blinkLed(){
+  unsigned long currentTime = millis();
+  if((currentTime-note1Time) >= 500){   
+    note1Time = currentTime;
+      Serial.println(digitalRead(RedPin));
+      if(ledState == false){
+        analogWrite(RedPin,valueOf8bit);
+        ledState = true;
+      }else{
+        digitalWrite(RedPin,LOW);
+        ledState = false;
+      }
+  }
 }
