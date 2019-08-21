75ad82f9d6d0315ca99f653c351f206c5d343355
徐國堂 <roberthsu2003@gmail.com>
2019-08-21 10:21:18 +0800

    modify

--- arduino/potentiometer/potentiometer.ino
+++ arduino/potentiometer/potentiometer.ino
@@ -1,15 +1,18 @@
-#define RedPin 13
+#define RedPin 6
 #define potPin A0
 
 void setup() {
   // put your setup code here, to run once:
 Serial.begin(115200);
 pinMode(RedPin,OUTPUT);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   int potValue = analogRead(potPin);
-  Serial.println(potValue);
-  delay(100);
+  int valueOf8bit = (float)potValue/1023 * 255;
+  Serial.println(valueOf8bit);
+  
+  delay(100);analogWrite(RedPin,valueOf8bit);
 }
