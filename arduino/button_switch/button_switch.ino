4edc2372d5d7770fe7ed0203cff0251d17739afc
徐國堂 <roberthsu2003@gmail.com>
2019-08-22 11:52:58 +0800

    Update button_switch.ino

--- arduino/button_switch/button_switch.ino
+++ arduino/button_switch/button_switch.ino
@@ -2,20 +2,30 @@
 
 boolean btnState;
 unsigned int switchCount = 0;
+unsigned int stateChangeCount = 0;
 
-void setup() { 
+void setup() {
   Serial.begin(115200);
-  pinMode(BtnPin,INPUT_PULLUP);
+  pinMode(BtnPin, INPUT_PULLUP);
   btnState = digitalRead(BtnPin);
 }
 
 void loop() {
   boolean nowState = digitalRead(BtnPin);
-  if(btnState != nowState){
-    btnState = nowState;
-    switchCount++;
+  if (btnState != nowState) {
+    delay(10);
+    if (nowState == digitalRead(BtnPin)) {
+      btnState = nowState;
+      stateChangeCount++;
+      switch (stateChangeCount % 2) {
+        case 0:
+          switchCount++;
+          break;
+      }
+    }
   }
 
+  Serial.print("you click:");
   Serial.println(switchCount);
-  delay(10);
+
 }
