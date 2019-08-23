48cb1f25edc61e1ebc0e4974d37933ba16bc426b
徐國堂 <roberthsu2003@gmail.com>
2019-08-23 09:50:14 +0800

    Create led.ino

---
+++ arduino/led/led.ino
@@ -0,0 +1,18 @@
+class Led{
+  public:
+   byte pinNum;
+};
+
+Led firstLed;
+Led secondLed;
+
+void setup() {
+  // put your setup code here, to run once:
+  firstLed.pinNum = 13;
+  secondLed.pinNum = 12;
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
