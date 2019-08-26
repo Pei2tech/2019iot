707c47563358118d2048086b023d49bd07ee0821
徐國堂 <roberthsu2003@gmail.com>
2019-08-26 11:48:24 +0800

    modify

--- arduino/button_switch/button_switch.ino
+++ arduino/button_switch/button_switch.ino
@@ -1,26 +1,47 @@
 #include "Led.h"
 #define BtnPin 2
 
-boolean btnState;
-unsigned int switchCount = 0;
-unsigned int stateChangeCount = 0;
+class SwitchButton{
+  private:
+    byte _pinNum;
+    unsigned long _stateChangeCount;
+    boolean _state;
+
+  public:
+    SwitchButton(byte pin){
+      _pinNum=pin;      
+      _stateChangeCount=0;
+      pinMode(_pinNum,INPUT_PULLUP);
+      _state = digitalRead(_pinNum);
+    }
+
+     unsigned long getStateChangeCount(){
+     boolean nowState = digitalRead(BtnPin);
+       if (_state != nowState) {
+            delay(10);
+        if (nowState == digitalRead(BtnPin)) {
+           _state = nowState;
+           _stateChangeCount++;
+        }
+    }
+    return _stateChangeCount;
+}
+};
+
+
+
+
 LED redLed(13);
+SwitchButton btn(2);
 
 void setup() {
-  Serial.begin(115200);
-  pinMode(BtnPin, INPUT_PULLUP);
-  btnState = digitalRead(BtnPin);
+  Serial.begin(115200); 
   redLed.off();
 }
 
 void loop() {
-  boolean nowState = digitalRead(BtnPin);
-  if (btnState != nowState) {
-    delay(10);
-    if (nowState == digitalRead(BtnPin)) {
-      btnState = nowState;
-      stateChangeCount++;
-      switch (stateChangeCount % 4) {
+ unsigned int stateChangeCount = btn.getStateChangeCount();
+    switch (stateChangeCount % 4) {
         case 0:
           redLed.off();
           break;
@@ -28,10 +49,10 @@ void loop() {
           redLed.on();
           break;
       }
-    }
-  }
+    
+
 
   Serial.print("you click:");
-  Serial.println(switchCount);
+  Serial.println(stateChangeCount/2);
 
 }
