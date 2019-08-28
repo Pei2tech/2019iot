9fc4f6e45153bfc5a0bd77344b8cc2e4b496d597
徐國堂 <roberthsu2003@gmail.com>
2019-08-28 09:35:09 +0800

    modify

---
+++ arduino/seven_segmented/seven_segmented.ino
@@ -0,0 +1,13 @@
+//***********************************************************
+//參考的網址
+//http://programmermagazine.github.io/201307/htm/article1.html
+//***********************************************************
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
