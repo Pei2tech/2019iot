683958f5174216fa66c4bc693681b6a59687f39a
徐國堂 <roberthsu2003@gmail.com>
2019-09-09 15:50:31 +0800

    modify

--- arduino/lm35_74CH595_4digit_seven_segmented/lm35_74CH595_4digit_seven_segmented.ino
+++ arduino/lm35_74CH595_4digit_seven_segmented/lm35_74CH595_4digit_seven_segmented.ino
@@ -1,9 +1,18 @@
-void setup() {
-  // put your setup code here, to run once:
 
+#define tempPin A0
+
+float tempC;
+
+
+void setup()
+{
+  Serial.begin(115200); //opens serial port, sets data rate to 9600 bps
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop()
+{
+  tempC = analogRead(tempPin);           //read the value from the sensor
+  tempC = (5.0 * tempC * 100.0) / 1024.0; //convert the analog data to temperature
+  Serial.println(tempC);
+  delay(1000);
 }
