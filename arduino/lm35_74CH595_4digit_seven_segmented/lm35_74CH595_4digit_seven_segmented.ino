9812d1096f094435c5da9f711594afc661248a78
徐國堂 <roberthsu2003@gmail.com>
2019-09-10 11:23:02 +0800

    Update lm35_74CH595_4digit_seven_segmented.ino

--- arduino/lm35_74CH595_4digit_seven_segmented/lm35_74CH595_4digit_seven_segmented.ino
+++ arduino/lm35_74CH595_4digit_seven_segmented/lm35_74CH595_4digit_seven_segmented.ino
@@ -9,18 +9,52 @@
 #define latchPin D6
 #define clockPin D7
 
-float tempC;
+
+
+const byte LEDs[10] = {
+  B01111110, //0
+  B00110000, //1
+  B01101101, //2
+  B01111001, //3
+  B00110011, //4
+  B01011011, //5
+  B01011111, //6
+  B01110000,//7
+  B01111111,//8
+  B01110011  //9
+};
 
 
 void setup()
 {
   Serial.begin(115200); //opens serial port, sets data rate to 9600 bps
+  pinMode(pin0,OUTPUT);
+  pinMode(pin1,OUTPUT);
+  pinMode(pin2,OUTPUT);
+  pinMode(pin3,OUTPUT);
+
+  pinMode(dataPin,OUTPUT);
+  pinMode(latchPin,OUTPUT);
+  pinMode(clockPin,OUTPUT);
 }
 
 void loop()
 {
-  tempC = analogRead(tempPin);           //read the value from the sensor
-  tempC = (tempC / 1024 * 3.3 / 0.01); //convert the analog data to temperature
-  Serial.println(tempC);
+ float tempC = getTemperature();
+ Serial.println(tempC);
+  digitalWrite(pin0,LOW);
+  digitalWrite(pin1,HIGH);
+  digitalWrite(pin2,HIGH);
+  digitalWrite(pin3,HIGH);
+  digitalWrite(latchPin, LOW);
+  shiftOut(dataPin, clockPin,LSBFIRST,LEDs[0]);
+  digitalWrite(latchPin, HIGH);
   delay(1000);
+}
+
+float getTemperature(){
+  float tempC;
+  tempC = analogRead(tempPin);           //read the value from the sensor
+  tempC = (tempC / 1024 * 3.3 / 0.01)-2;
+  return tempC;
 }
