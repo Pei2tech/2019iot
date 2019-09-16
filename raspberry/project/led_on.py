53dab7f34499337961076a65f234c2a6028f7233
robertHsu <roberthsu2003@gmail.com>
2019-09-16 15:32:16 +0800

    modify

--- raspberry/project/led_on.py
+++ raspberry/project/led_on.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-
+#https://sites.google.com/site/rasberrypintust/raspberry-pi-shu-mei-pai-shi-qian-zhun-bei/python-gipo-an-zhuang
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 GPIO.cleanup()
-GPIO.setwarnings(false)
+GPIO.setwarnings(False)
 GPIO.setup(17, GPIO.OUT)
 GPIO.setup(27, GPIO.OUT)
 print("17 27亮")
