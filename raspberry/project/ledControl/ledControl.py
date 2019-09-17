24f032f60639d83c4960d4133da3a8f5d3585b80
robertHsu <roberthsu2003@gmail.com>
2019-09-17 13:17:44 +0800

    modify

--- raspberry/project/ledControl/ledControl.py
+++ raspberry/project/ledControl/ledControl.py
@@ -1,13 +1,18 @@
 from tkinter import *
 
-def appInterface(window):
-    frame = Frame(window,borderwidth=2,width=500,height=600,relief=GROOVE);
-    frame.pack(padx=10,pady=10);
+def userClick():
+    print("user click");
     pass
+
+def appInterface(window):
+    frame = Frame(window,borderwidth=2,relief=GROOVE);
+    ledButton = Button(frame,text="ON",padx=30,pady=10,command=userClick).pack(padx=40,pady=40);
+    frame.pack(padx=10,pady=10);
+    
 
 if __name__ == '__main__':
     app = Tk();
     app.title("LEDControl");
-    app.geometry("500x600");
+    #app.geometry("500x600");
     appInterface(window = app);
     app.mainloop();
