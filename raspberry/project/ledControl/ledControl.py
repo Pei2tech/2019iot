33ca69bafe6adff034b014605ead4f22a805c5ef
robertHsu <roberthsu2003@gmail.com>
2019-09-17 14:28:57 +0800

    modify

--- raspberry/project/ledControl/ledControl.py
+++ raspberry/project/ledControl/ledControl.py
@@ -1,3 +1,6 @@
+#! /usr/bin/python3
+
+#pip3 install RPi.GPIO
 from tkinter import *
 import RPi.GPIO as GPIO
 
@@ -17,6 +20,7 @@ def userClick():
     else:
         buttonText.set('ON')
         GPIO.output(25, GPIO.LOW)
+    
     
 
 if __name__ == '__main__':
