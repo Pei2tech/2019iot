f57611789ee6f942b5904991e3333c75da16e2ef
robertHsu <roberthsu2003@gmail.com>
2019-09-19 11:10:40 +0800

    modify

---
+++ raspberry/project/mcp3008/window_channel0.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from gpiozero import MCP3008
+
+class MCP3008app:
+    pass;
+
+if __name__ == "__main__":
+    window = Tk();
+    window.title("MCP3008");
+    app = MCP3008app();
+    window.mainloop;
