b5508201a99da743272b6a1763ad6c086761a753
robertHsu <roberthsu2003@gmail.com>
2019-09-19 14:18:17 +0800

    modify

--- raspberry/project/mcp3008/window_channel0.py
+++ raspberry/project/mcp3008/window_channel0.py
@@ -1,14 +1,22 @@
 from tkinter import *
 from gpiozero import MCP3008
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
 
 class MCP3008app:
     def __init__(self,w):
         self.window = w;
+        cred = credentials.Certificate('/home/pi/Documents/raspberryfirebase-firebase-adminsdk-q4ht6-3282b25b5b.json')
+        firebase_admin.initialize_app(cred, {'databaseURL': 'https://raspberryfirebase.firebaseio.com/',
+                                             'databaseAuthVariableOverride': {'uid': 'NatxhTYp4AaiI2Uge648cNQjSIE3'}
+                                             })
+        #self.registerRef = ref = db.reference('/some_resource')
         self.createGUI();
         
     def createGUI(self):
         mainFrame = Frame(self.window,borderwidth=2,relief=GROOVE);
-        Label(mainFrame,text="可變電阻:").pack();
+        Label(mainFrame,text="可變電阻:").pack(anchor=W,padx=5,pady=5);
         scaleValue = DoubleVar();
         scaleValue.set(67)
         Scale(mainFrame,from_=0, to=100,variable=scaleValue, orient=HORIZONTAL,length=400,state=DISABLED).pack()
