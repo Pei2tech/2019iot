1ca35e4b3b44fce2512b5b3776639dd94bb3da4f
robertHsu <roberthsu2003@gmail.com>
2019-09-19 15:02:49 +0800

    modify

--- raspberry/project/mcp3008/window_channel0.py
+++ raspberry/project/mcp3008/window_channel0.py
@@ -3,10 +3,12 @@ from gpiozero import MCP3008
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
+from threading import Timer
 
 class MCP3008app:
     def __init__(self,w):
         self.window = w;
+        self.channel0 = MCP3008(0,max_voltage=5.0);
         cred = credentials.Certificate('/home/pi/Documents/raspberryfirebase-firebase-adminsdk-q4ht6-3282b25b5b.json')
         firebase_admin.initialize_app(cred, {'databaseURL': 'https://raspberryfirebase.firebaseio.com/',
                                              'databaseAuthVariableOverride': {'uid': 'NatxhTYp4AaiI2Uge648cNQjSIE3'}
@@ -14,16 +16,22 @@ class MCP3008app:
         self.registerRef  = db.reference('iot0624/MCP3008/register')
         print(self.registerRef.get())
         self.createGUI();
+        self.checkRegister();
+        
         
     def createGUI(self):
         mainFrame = Frame(self.window,borderwidth=2,relief=GROOVE);
         Label(mainFrame,text="可變電阻:").pack(anchor=W,padx=5,pady=5);
-        scaleValue = DoubleVar();
-        scaleValue.set(67)
-        Scale(mainFrame,from_=0, to=100,variable=scaleValue, orient=HORIZONTAL,length=400,state=DISABLED).pack()
+        self.scaleValue = DoubleVar();
+        self.scaleValue.set(67)
+        Scale(mainFrame,from_=0, to=100,variable=self.scaleValue, orient=HORIZONTAL,length=400,state=DISABLED).pack()
         
         
         mainFrame.pack(padx=30,pady=30);
+    
+    def checkRegister(self):
+        value = self.channel0.value;
+        print("now value:{}".format(value));
 
 
 if __name__ == "__main__":
