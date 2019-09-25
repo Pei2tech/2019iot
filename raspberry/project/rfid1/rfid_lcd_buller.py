0cf7dbe31dd97995c6469a22c8f96575331c6247
robertHsu <roberthsu2003@gmail.com>
2019-09-25 10:13:06 +0800

    modify

--- raspberry/project/rfid1/rfid_lcd_buller.py
+++ raspberry/project/rfid1/rfid_lcd_buller.py
@@ -1,4 +1,16 @@
 from tkinter import *
+class App:
+    def __init__(self,window):
+        #buzzer button
+        buzzerFrame = Frame(window);
+        Button(buzzerFrame,text="Buzzer Control", padx=10, pady=10,command=self.userClickBuzzer).pack(padx=30,pady=10);
+        buzzerFrame.pack();
+        
+        #buzzer init
+        
+    def userClickBuzzer(self):
+        print("user click");
+        pass;
 
 if __name__ == "__main__":
     root = Tk();
@@ -6,5 +18,5 @@ if __name__ == "__main__":
     root.option_add("*font",("verdana", 18, "bold"));
     root.option_add("*background", "gold");
     root.option_add("*forground","#888888");
-    
+    app = App(root);
     root.mainloop();
