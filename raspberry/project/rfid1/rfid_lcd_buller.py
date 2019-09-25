96e0cc334751cd8b1a6ac92af54f8d4003efaddb
robertHsu <roberthsu2003@gmail.com>
2019-09-25 09:48:06 +0800

    modify

---
+++ raspberry/project/rfid1/rfid_lcd_buller.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+if __name__ == "__main__":
+    root = Tk();
+    root.title("RFID_LCD_BUZZER");
+    root.option_add("*font",("verdana", 18, "bold"));
+    root.option_add("*background", "gold");
+    root.option_add("*forground","#888888");
+    
+    root.mainloop();
